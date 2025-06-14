5b05cfcb9d1c166b5a8184556942baa35fda9d38
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-14 14:23:20 +0530

    Create 315. Count of Smaller Numbers After Self.java

---
+++ sorting/315. Count of Smaller Numbers After Self.java
@@ -0,0 +1,80 @@
+class Pair {
+    int ele;
+    int idx;
+
+    public Pair (int ele, int idx) {
+        this.ele = ele;
+        this.idx = idx;
+    }
+}
+
+class Solution {
+    private void mergeSortedArray(Pair p[], int low, int mid, int high, int ans[]) {
+        int i = low;
+        int j = mid+1;
+        int n1 = mid+1;
+        int n2 = high+1;
+
+        List<Pair> temp = new ArrayList<>();
+        int count = 0;
+
+        while(i < n1 && j < n2) {
+            if (p[i].ele > p[j].ele) {
+                count++;
+                temp.add(p[j]);
+                j++;
+            } else {
+                ans[p[i].idx] += count;
+                temp.add(p[i]);
+                i++;
+            }
+        }
+
+        while(i < n1) {
+            ans[p[i].idx] += count;
+            temp.add(p[i]);
+            i++;
+        }
+
+        while (j < n2) {
+            temp.add(p[j]);
+            j++;
+        }
+
+        int pos = low;
+        for(Pair pp : temp) {
+            p[pos] = pp;
+            pos++;
+        }
+    }
+
+    private void mergeSort(Pair p[], int low, int high, int ans[]) {
+        if (low >= high) return;
+
+        int mid = (low + high)/2;
+        mergeSort(p,low,mid,ans);
+        mergeSort(p,mid+1,high,ans);
+
+        mergeSortedArray(p,low,mid,high,ans);
+    }
+
+    public List<Integer> countSmaller(int[] nums) {
+
+        int n = nums.length;
+        Pair p [] = new Pair[n];
+
+        for(int i=0;i<n;i++) {
+            p[i] = new Pair(nums[i], i); 
+        }
+
+        int ans [] = new int[n];
+        mergeSort(p,0,n-1,ans);
+
+        List<Integer> res = new ArrayList<>();
+        for(int ele : ans) {
+            res.add(ele);
+        }
+
+        return res;
+    }
+}
