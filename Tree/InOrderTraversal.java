c624af7f8dbcba82e9ba701b613261a84dbc2160
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-07-26 13:00:25 +0530

    Create InOrderTraversal.java

---
+++ Tree/InOrderTraversal.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private void helper(TreeNode root, ArrayList<Integer> ans) {
+        if (root == null) { // When the root becomes null -> return from there
+            return;
+        }
+
+        helper(root.left, ans); // calling to the left child
+        ans.add(root.val); // store the answer the in the arraylist
+        helper(root.right, ans); // calling to the right child
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        helper(root, ans);
+        return ans;
+    }
+}
