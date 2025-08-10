54efc4ce6d8ad2095ed2acae237d650629a8e5ee
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-10 14:05:06 +0530

    Update 307. Range Sum Query - Mutable.java

--- Tries and Segment Tree/307. Range Sum Query - Mutable.java
+++ Tries and Segment Tree/307. Range Sum Query - Mutable.java
@@ -20,18 +20,48 @@ class NumArray {
 
         segmentTree[i] = segmentTree[2*i+1] + segmentTree[2*i+2];
     }
+
+    private void updateSegmentTree(int i, int l, int r, int index, int val) {
+        if (l == r) {
+            segmentTree[i] = val;
+            return;
+        }
+
+        int mid = (l+r)/2;
+
+        if (index <= mid) {
+            updateSegmentTree(2*i+1,l,mid,index,val);
+        } else {
+            updateSegmentTree(2*i+2,mid+1,r,index,val);
+        }
+
+        segmentTree[i] = segmentTree[2*i+1] + segmentTree[2*i+2];
+    }
     
     public void update(int index, int val) {
-        nums[index] = val;
+        updateSegmentTree(0,0,n-1,index,val);
+    }
+
+    private int querySegmentTree(int i, int l, int r, int start, int end) {
+        if (l > end || r < start) {
+            return 0;
+        }
+
+        if (start <= l && r <= end) {
+            return segmentTree[i];
+        }
+
+        int mid = (l+r)/2;
+
+        int leftHalfSum = querySegmentTree(2*i+1,l,mid,start,end);
+        int rightHalfSum = querySegmentTree(2*i+2,mid+1,r,start,end);
+
+        return leftHalfSum + rightHalfSum;
+
     }
     
     public int sumRange(int left, int right) {
-        int sum = 0;
-        for(int i=left;i<=right;i++) {
-            sum += nums[i];
-        }
-
-        return sum;
+        return querySegmentTree(0,0,n-1,left,right);
     }
 }
 
